10dcc9fb84a2f92b8954327f74c1741e7e32ee60
Nikolay Gagarinov <feycot@gmail.com>
2025-11-30 20:02:36 +0500

    --wip-- [skip ci]

--- src/game.ts
+++ src/game.ts
@@ -1,41 +1,87 @@
+type Position = {
+  x: number,
+  y: number,
+}
+
+type Direction = {
+  dx: number,
+  dy: number,
+}
+
+type Ball = {
+  radius: number
+  position: Position,
+}
+
+type CanvasConfig = {
+  width: number,
+  height: number,
+}
+
+type GameData = {
+  ball: Ball
+}
 
 const startGame = (): void => {
-  const state = {
+  const canvasConfig: CanvasConfig = {
+    width: 480,
+    height: 320
   }
+
   const root = document.querySelector<HTMLDivElement>(('#app'))!
-  const canvas = makeCanvas()
+  const canvas = makeCanvas(canvasConfig)
   const ctx = canvas.getContext('2d')!
   root.append(canvas)
 
-  const ballPosition = {
-    x: canvas.width / 2,
-    y: canvas.height - 30,
+  const state: GameData = {
+    ball: {
+      radius: 10,
+      position: {
+        x: canvas.width / 2,
+        y: canvas.height - 30,
+      }
+    }
   }
 
   setInterval(() => {
-    ctx.clearRect(0, 0, 480, 320);
+    ctx.clearRect(0, 0, canvas.height, canvas.width);
 
-    drawBall(ctx, ballPosition)
+    drawBall(ctx, state.ball)
 
-    ballPosition.x += 2
-    ballPosition.y += -2
+    move(state.ball, {dx: 2, dy: -2})
   }, 16.7) // 60 frames per sec
+  setInterval(() => {
+    console.log(state.ball.position.y)
+  }, 1000)
 }
 
-const makeCanvas = (): HTMLCanvasElement => {
+const makeCanvas = (config): HTMLCanvasElement => {
   const canvas = document.createElement('canvas')
-  canvas.setAttribute('width', '480')
-  canvas.setAttribute('height', '320')
+  canvas.setAttribute('width', config.width)
+  canvas.setAttribute('height', config.height)
 
   return canvas
 }
 
-const drawBall = (ctx: CanvasRenderingContext2D, { x, y}) => {
+const drawBall = (ctx: CanvasRenderingContext2D, ball: Ball) => {
+  const { position } = ball;
+
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(position.x, position.y, ball.radius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
 }
 
 export default startGame;
+
+
+
+const draw = (canvas: HTMLCanvasElement, state: object): void => {
+
+}
+
+const move = (figure: any, direction: Direction): void => {
+  figure.position += direction.dx
+  figure.position += direction.dy
+}
